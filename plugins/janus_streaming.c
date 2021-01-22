64da1e3d28f34f5e9a281fa22144573336ce9dc4
Dave <dhoofnagle@whalerockindustries.com>
2021-01-21 22:51:28 -0600

    remove json_string
    just use mp->description

--- plugins/janus_streaming.c
+++ plugins/janus_streaming.c
@@ -3767,9 +3767,7 @@ static json_t *janus_streaming_process_synchronous_request(janus_streaming_sessi
 		json_t *event = json_object();
 		json_object_set_new(event, "streaming", json_string("event"));
 		/* updated hoofsc */
-		JANUS_LOG(LOG_INFO, "CHECK 1: mp->desc %s\n", mp->description);
-		JANUS_LOG(LOG_INFO, "CHECK 2: json_string(mp->desc) %s\n", json_string(mp->description));
-		json_object_set_new(event, "description", json_string(mp->description));
+		json_object_set_new(event, "description", mp->description);
 		/* end updated */
 		json_t *result = json_object();
 		json_object_set_new(result, "status", json_string("stopped"));
